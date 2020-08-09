770f6902a329c4d4a66d0e653855d056bd3fe5fa
Trung Vo <trung.vo@seldatinc.com>
2020-08-09 15:13:39 +0700

    - update network structure

---
+++ Surveys/Networks/NetworkManager.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkManager.swift
+//  Surveys
+//
+//  Created by Trung Vo on 8/7/20.
+//  Copyright © 2020 Trung Vo. All rights reserved.
+//
+
+import Foundation
+import Moya
+import os.log
+
+enum JsonParseError: Error {
+    case couldNotDecode
+    
+    var errorDescription: String {
+        switch self {
+        case .couldNotDecode:
+            return Messages.CouldNotDecode
+        }
+        
+    }
+}
+
+class NetworkManager: Networkable {
+    let authPlugin = AccessTokenPlugin { (_) -> String in
+        return appKeyChain.accessToken
+    }
+    
+    lazy var provider = MoyaProvider<MultiTarget>(plugins: [authPlugin])
+    
+    private var decoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+    
+    func jsonDecode<T>(_ type: T.Type, fromData data: Data) throws -> T where T: Decodable {
+        guard let result: T = try? decoder.decode(type, from: data) else {
+            throw JsonParseError.couldNotDecode }
+        
+        return result
+    }
+}
